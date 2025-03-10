e10513eee70c73fc5d3c253ee34cf4696f908805
Kittipop420012 <kittipop.thon@bumail.net>
2025-03-10 10:03:28 +0700

    code

---
+++ Assets/Script/Gravity.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+    Rigidbody rb;
+    const float G = 0.00674f;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    void Attract(Gravity other)
+    {
+        Rigidbody rbOther = GetComponent<Rigidbody>();
+
+        Vector3 direction = rb.position - other.rb.position;
+
+        float distance = direction.magnitude;
+
+        if(distance == 0) { return; }
+
+        float forceMagnitude = G * ((rb.mass * other.rb.mass)/ Mathf.Pow(distance, 2)) ;
+        Vector3 force = forceMagnitude * direction.normalized;
+    }
+    
+}
